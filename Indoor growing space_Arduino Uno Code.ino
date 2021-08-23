a976bd012f343f522653873c7d879dbe3b06ea9c
ReemDal <89409077+ReemDal@users.noreply.github.com>
2021-08-23 13:53:07 -0300

    Update Indoor growing space_Arduino Uno Code.ino

--- Indoor growing space_Arduino Uno Code.ino
+++ Indoor growing space_Arduino Uno Code.ino
@@ -1,1 +1,45 @@
+#include <Wire.h>
 
+#include <Wire.h>
+#include "SHT2x.h"
+
+const int analogInPin = A0;  // Analog input pin for Apogee 515
+
+int period = 1000;
+unsigned long time_now = 0;
+
+SHT2x SHT2x;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //Start the Serial Port at 9600 baud (default)
+  Wire.begin();
+}
+
+void loop() {
+  if(millis() > time_now + period){
+        time_now = millis();
+
+      // read the input from analog pin:
+      int sensorValue = analogRead(analogInPin);
+      
+      // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+      float SQ515a = sensorValue * (5.0 / 1023.0);
+      
+      //Multiply this calibration factor by the measured voltage to convert sensor output to PPFD 
+      float SQ515b = SQ515a * 2000;
+      int SQ515 = int(SQ515b);
+      
+      float T = SHT2x.GetTemperature();
+      float H = SHT2x.GetHumidity();
+         
+      Serial.print(T);
+      Serial.print(",");
+      Serial.print(H);
+      Serial.print(",");
+      Serial.print(SQ515);
+      Serial.print(",");
+      Serial.println();
+  }
+
+}
